07f5c2284b9e11d69f11fbb9e8aea1bf4d30f582
Nils Weller <nils@deepcode.ai>
2019-11-12 15:06:43 +0100

    test

---
+++ test42.java
@@ -0,0 +1,14 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class test {
+	public static void main(String [] args) {
+		try {
+		new BufferedReader(new FileReader("gnu"));
+		} catch (Exception e) {}
+		try {
+		new BufferedReader(new FileReader("gnu"));
+		} catch (Exception e) {}
+	}
+}
+//42
